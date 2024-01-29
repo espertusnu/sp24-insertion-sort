ababf2ebff79c20b9967101d6d913fef0a37ee96
Ellen Spertus <ellen.spertus@gmail.com>
2024-01-28 20:07:54 -0800

    Ensure list is mutable

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sorter {
-    static void sortInts(List<Integer> list) {
+    static void sortInts(ArrayList<Integer> list) {
         for (int i = 1; i < list.size(); i++) {
             int value = list.get(i);
 
@@ -19,7 +19,7 @@ public class Sorter {
     }
 
     public static void main(String[] args) {
-        List<Integer> ints = new ArrayList<>(List.of(3, 12, 7, 4));
+        ArrayList<Integer> ints = new ArrayList<>(List.of(3, 12, 7, 4));
         System.out.println(ints);
         sortInts(ints);
         System.out.println(ints);
