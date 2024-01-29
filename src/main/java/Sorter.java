d1878c338ceb3251576d2f2df5776b4c210846ec
Ellen Spertus <ellen.spertus@gmail.com>
2024-01-28 20:24:37 -0800

    Generalize sort

--- src/main/java/Sorter.java
+++ src/main/java/Sorter.java
@@ -2,13 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Sorter {
-    static void sortInts(ArrayList<Integer> list) {
+    static  <T extends Comparable<T>> void sort(ArrayList<T> list) {
         for (int i = 1; i < list.size(); i++) {
-            int value = list.get(i);
+            T value = list.get(i);
 
             // Move elements greater than value to higher indices.
             int j = i - 1;
-            while (j >= 0 && list.get(j) > value) {
+            while (j >= 0 && list.get(j).compareTo(value) > 0) {
                 list.set(j + 1, list.get(j));
                 j--;
             }
@@ -21,7 +21,7 @@ public class Sorter {
     public static void main(String[] args) {
         ArrayList<Integer> ints = new ArrayList<>(List.of(3, 12, 7, 4));
         System.out.println(ints);
-        sortInts(ints);
+        sort(ints);
         System.out.println(ints);
     }
 }
